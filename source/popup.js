9ed591d68e10d0fd2124e9978329fa19158787de
Daniel Zheng <danielzheng1989@gmail.com>
2017-08-25 23:42:15 +0200

    added: capture image of active tab

--- source/popup.js
+++ source/popup.js
@@ -3,15 +3,35 @@
  */
 angular.module("cassia", []);
 
-angular.module("cassia").directive("cassiaTest", function () {
+angular.module("cassia").directive("cassiaTest", ["cassiaTabs", function (cassiaTabs) {
 	return {
 		restrict: "E",
 		replace: true,
 		template: `<span>test</span>`,
 		link: function ($scope, $element, $attributes, $controller) {
+
+			(async () => {
+				var dataUrl = await cassiaTabs.captureTab();
+				console.log(dataUrl);
+				$element.append(`<img src="${dataUrl}" />`);
+			})();
 		}
 	};
-});
+}]);
+
+//https://developer.chrome.com/extensions/tabs#method-captureVisibleTab
+angular.module("cassia").service("cassiaTabs", [function () {
+	if (!chrome.tabs) throw "chrome.tabs not supported";
+	var self = this;
+
+	self.captureTab = () => {
+		return new Promise((resolve, reject) => {
+			chrome.tabs.captureVisibleTab(resolve);
+		});
+	};
+
+	return self;
+}]);
 
 angular.element(document).ready(function () {
 	var injector = angular.element(document).injector();
